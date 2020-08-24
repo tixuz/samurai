8b63650d6b0b99a7a1fe9246cc80bf2d3447f9f9
tixuz <hindolmirzo@gmail.com>
2020-08-24 10:48:44 +0500

    Lesson 006. app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div>One div</div>
+        <div className="App">
+          <ul>
+            <li>html</li>
+            <li>css</li>
+            <li>js</li>
+            <li>react</li>
+          </ul>
+
+        </div>
+      </div>
   );
 }
 

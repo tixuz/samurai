2ab1823358abcf4db077d24ee1dbc00ce7cdcfc1
tixuz <hindolmirzo@gmail.com>
2020-08-24 16:18:59 +0500

    Lesson 014. import classes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from "./Header.module.css"
 
 
 const Header = () => {
     return (
-        <header className='header'><img width='75px'
+        <header className={s.header}><img
                                             src="https://images.vexels.com/media/users/3/142676/isolated/preview/65b05478c5062b6b1cccee12e0f093b4-house-swirls-icon-by-vexels.png"
                                             alt='logo'/></header>
           );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import s from "./Profile.module.css"
 
 
 const Profile = () => {
     return (
 
-            <div className='content'>
+            <div className={s.content}>
 
                 <div>
                     <img src='https://cdn.pixabay.com/photo/2014/04/03/10/25/beach-310419_960_720.png' alt='beach'/>
@@ -18,10 +19,10 @@ const Profile = () => {
                         new post
                     </div>
                     <div>
-                        <div>
+                        <div className={s.item}>
                             post 1
                         </div>
-                        <div>
+                        <div className={s.item}>
                             post 2
                         </div>
                     </div>

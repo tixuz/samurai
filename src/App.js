7a0d37ca2081b6fe9fb0efed2bea78c0a6b21f61
tixuz <hindolmirzo@gmail.com>
2020-08-24 12:39:05 +0500

    Lesson 008-009. Components in outer file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import Header from './Header'
+import Technologies from './Technologies'
 
 const App = () => {
   return (
@@ -9,33 +11,6 @@ const App = () => {
       </div>
   );
 };
-
-const Header = () => {
-  return (
-      <div>
-        <a href="#s">Home</a>
-        <a href="#s">News feed</a>
-        <a href="#s">Messages</a>
-  </div>
-)
-};
-const Technologies = () => {
-  return (
-      <div>
-    <div>One div</div>
-    <div className="App">
-      <ul>
-        <li>html</li>
-        <li>css</li>
-        <li>js</li>
-        <li>react</li>
-      </ul>
-
-    </div>
-  </div>
-)
-};
-
 
 
 export default App;
---
+++ src/Technologies.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Technologies = () => {
+  return (
+      <div>
+    <div>One div</div>
+    <div className="App">
+      <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+
+    </div>
+  </div>
+)
+};
+
+
+
+export default Technologies;

3790f1776bbc29bfe24bd0ff9d6eee728db37640
tixuz <hindolmirzo@gmail.com>
2020-08-24 12:02:48 +0500

    Lesson 007. Components in one file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
       <div>
-        <div>One div</div>
-        <div className="App">
-          <ul>
-            <li>html</li>
-            <li>css</li>
-            <li>js</li>
-            <li>react</li>
-          </ul>
-
-        </div>
+        <Header/>
+        <Technologies/>
       </div>
   );
-}
+};
+
+const Header = () => {
+  return (
+      <div>
+        <a href="#s">Home</a>
+        <a href="#s">News feed</a>
+        <a href="#s">Messages</a>
+  </div>
+)
+};
+const Technologies = () => {
+  return (
+      <div>
+    <div>One div</div>
+    <div className="App">
+      <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+
+    </div>
+  </div>
+)
+};
+
+
 
 export default App;
